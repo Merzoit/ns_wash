2c350fd7284e072873f8f27f76b4e42df4d73993
Merzoit <68446392+Merzoit@users.noreply.github.com>
2022-11-07 20:25:58 +0300

    Start project
    Add some view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
+
+class LoginView(LoginView):
+    """ View for users auth """
+    pass
+
+
+class LogoutView(LogoutView):
+    """ View for logout """
+    pass
+
+
--- ns_wash/settings.py
+++ ns_wash/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
